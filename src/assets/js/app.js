f6229ea74aa7789f6525874fc1b42ab4dbd561d6
adityasharma2020 <aditya15116617@gmail.com>
2023-07-12 02:58:04 +0530

    gsap code added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,13 +1,48 @@
+import gsap from "gsap";
+
 const bar = document.querySelector(".loading__bar--inner");
 const counter_num = document.querySelector(".loading__counter--number");
-let c=0;
+let c = 0;
 
-let barInterval = setInterval(()=>{
-  bar.style.width = c+"%";
-  counter_num.innerText =c+"%";
+let barInterval = setInterval(() => {
+  bar.style.width = c + "%";
+  counter_num.innerText = c + "%";
   c++;
-  if(c===101){
+  if (c === 101) {
     clearInterval(barInterval);
-  }
-},20);
 
+    gsap.to(".loading__bar", {
+      duration: 3,
+      rotate: "90deg",
+      left: "300%",
+    });
+
+    gsap.to(".loading__text, .loading__counter", {
+      duration: 1,
+      opacity: 0,
+    });
+    gsap.to("..loading__box", {
+      duration: 2,
+      height: "500px",
+      borderRadius: "50%",
+    });
+    gsap.to("..loading__svg", {
+      duration: 10,
+      opacity: 1,
+      rotate: "360deg",
+    });
+
+    gsap.to("..loading__box", {
+      delay: 2,
+      border: none,
+      rotate: "360deg",
+    });
+
+    gsap.to("..loading", {
+     delay:2,
+     duration:2,
+     background:"transparent",
+     opacity:0.5
+    });
+  }
+}, 20);
