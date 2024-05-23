33602b1d20f21963be556a2c369ba7008e8bd41a
adityasharma2020 <aditya15116617@gmail.com>
2024-05-23 22:34:31 +0530

    new projects updated

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,41 +1,31 @@
-import images from "./images";
-
+import images from './images';
 
 let reviews = [
-  {
-    name: "Company: IDYLL",
-    position: "position: Web Developer Intern",
-    image: images.avatar1,
-    review:
-      `As a Web Development Intern at IDYLL from April 2023-September 2023 in Delhi i
+	{
+		name: 'Company: MIDAS Labs, IIIT Delhi',
+		position: 'position: Web Developer Intern',
+		image: images.avatar0,
+		review: `During my internship at MIDAS Labs (Oct 2023 - Apr 2024) as a Frontend Developer, I worked on the Audino project, an open-source audio annotation tool. I implemented features for organization management using React, React Query, Zustand, and Docker, and developed advanced UIs for AI-powered tools like text-to-voice and voice-to-text. Additionally, I built a landing page with Astro.js, resulting in faster load times and improved efficiency.`,
+	},
+	{
+		name: 'Company: IDYLL',
+		position: 'position: Web Developer Intern',
+		image: images.avatar1,
+		review: `During my internship at Idyll (Apr 2023 - Sep 2023) as a Full Stack Developer, I designed RESTful APIs and implemented secure authentication mechanisms. I worked on containerization using Docker and automated testing with Jest. Additionally, I integrated Git hooks, comprehensive error handling, and Winston logging, adhering to Test-Driven Development principles to ensure maintainable code.`,
+	},
 
-      Developed and maintained React-based frontend.
-      Worked on Node.js backend, building APIs.
-      Gained insights into industry-standard coding practices.
-      Learned AWS autoscaling and scalable architecture.
-      Acquired knowledge in microservices architecture and orchestration.
-      which Result in Enhanced app performance and stability.`,
-  },
-  {
-    name: "Company: NADOS: Pepcoding",
-    position: "position: Frontend developer intern",
-    image: images.avatar2,
-    review:
-      `During my NADOS:PEPCODING internship (Mar 2022-Sept 2022) in Delhi, I specialized in frontend development for their student-focused social media platform. I applied React, utilizing features like Hooks, Redux, and dynamic UI components. This experience also honed my expertise in authentication mechanisms for secure login and data protection. I consistently adhered to industry best practices and MVC architecture, while actively collaborating using Git and GitHub.`,
-  },
-  {
-    name: "Company: N Computing (Rajasthan IT)",
-    position: "position: Part-time Website Developer",
-    image: images.avatar3,
-    review: `As a part-time Web Developer at RajasthanIT Center, a franchise of NComputing (Jan 2021 - Jun 2021), I designed and developed a modern multi-page landing website to showcase their product line. I deployed the website, optimized it for search engines, and contributed to their online success. Skills: Bootstrap, Web Hosting,godaddy, cloudflare, Hostinger, HTML5, CSS, JavaScript.`,
-  },
-  {
-    name: "Company: Shri Balaji Dying",
-    position: "position: freelancer",
-    image: images.avatar4,
-    review:
-      "As a Freelance Web Developer (Aug 2021 - Oct 2021), I partnered with Shri Balaji Dying to craft a dynamic e-commerce site, enhancing user experience, driving sales, and boosting revenue. My expertise in WordPress, HTML5, CSS, and JavaScript guarantees innovative web solutions.",
-  },
+	{
+		name: 'Company: N Computing (Rajasthan IT)',
+		position: 'position: Part-time Website Developer',
+		image: images.avatar3,
+		review: `As a part-time Web Developer at RajasthanIT Center, a franchise of NComputing (Jan 2021 - Jun 2021), I designed and developed a modern multi-page landing website to showcase their product line. I deployed the website, optimized it for search engines, and contributed to their online success. Skills: Bootstrap, Web Hosting,godaddy, cloudflare, Hostinger, HTML5, CSS, JavaScript.`,
+	},
+	{
+		name: 'Company: Shri Balaji Dying',
+		position: 'position: freelancer',
+		image: images.avatar4,
+		review: 'As a Freelance Web Developer (Aug 2021 - Oct 2021), I partnered with Shri Balaji Dying to craft a dynamic e-commerce site, enhancing user experience, driving sales, and boosting revenue. My expertise in WordPress, HTML5, CSS, and JavaScript guarantees innovative web solutions.',
+	},
 ];
 
 export { reviews };
--- src/assets/js/images.js
+++ src/assets/js/images.js
@@ -1,17 +1,19 @@
-import avatar1 from "../images/idyll_logo.jpg";
-import avatar2 from "../images/pepcoding_logo.jpg";
-import avatar3 from "../images/ncomputing_logo.png";
-import avatar4 from "../images/shree_balaiji_logo_circle.png";
-import avatar5 from "../images/avatars/5.webp";
-import avatar6 from "../images/avatars/6.webp";
+import avatar0 from '../images/Midaslogo.jpeg';
+import avatar1 from '../images/idyll_logo.jpg';
+import avatar2 from '../images/pepcoding_logo.jpg';
+import avatar3 from '../images/ncomputing_logo.png';
+import avatar4 from '../images/shree_balaiji_logo_circle.png';
+import avatar5 from '../images/avatars/5.webp';
+import avatar6 from '../images/avatars/6.webp';
 
 const images = {
-  avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-  avatar6,
+	avatar0,
+	avatar1,
+	avatar2,
+	avatar3,
+	avatar4,
+	avatar5,
+	avatar6,
 };
 
 export default images;
